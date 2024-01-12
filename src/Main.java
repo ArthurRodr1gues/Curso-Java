1dcd8061c646d4f185994a24c0c5f44b21fff13a
ArthurRodr1gues <doc.estudos@gmail.com>
2024-01-12 17:19:22 -0300

    Processamento(atribuição) de dados e Casting (conversão de tipo)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        float b, B, h, area;
+        b = 6f;//Essse f ao lado das variáveis é uma boa prática, pois indica que o número é um float
+        B = 8f;
+        h = 5f;
+        area = (b + B) / 2f * h;
+        System.out.println(area);
+
+    }
+}
