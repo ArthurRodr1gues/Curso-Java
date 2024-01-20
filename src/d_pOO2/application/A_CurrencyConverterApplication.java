5345b1eab3d3c428d58dd0301f6ad055bc676413
ArthurRodr1gues <doc.estudos@gmail.com>
2024-01-20 16:40:36 -0300

    Capítulo 8 e 9
     Construtores, palavra this, sobrecarga, encapsulamento >> Account. Membros estáticos >> CurrencyConverter

---
+++ src/d_pOO2/application/A_CurrencyConverterApplication.java
@@ -0,0 +1,24 @@
+package d_pOO2.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import d_pOO2.entities.A_CurrencyConverter;
+
+
+public class A_CurrencyConverterApplication {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is the dollar price? ");
+        double dollarPrice = sc.nextDouble();
+        System.out.print("How many dollars will be bought? ");
+        double amount = sc.nextDouble();
+        
+        double result = A_CurrencyConverter.dollarToReal(amount, dollarPrice);
+        
+        System.out.printf("Amount to be paid in reais = %.2f%n", result);
+        sc.close();
+        }    
+}
---
+++ src/d_pOO2/entities/A_CurrencyConverter.java
@@ -0,0 +1,9 @@
+package d_pOO2.entities;
+
+public class A_CurrencyConverter {
+    public static double IOF = 0.06;
+    
+    public static double dollarToReal(double amount, double dollarPrice) {
+        return amount * dollarPrice * (1.0 + IOF);
+    }
+}
